df0ecdcee6cf4f7523f3a1da6927d9acad396d93
sanotogii <nasaereh@gmail.com>
2024-03-22 17:56:27 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,16 @@
 # install flask from pip3 with puppet
-package { 'python3-pip':
-  ensure => installed,
+package {'python3-pip':
+ensure  => 'installed',
+}
+exec { 'install_werkzeug':
+command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+path    => '/usr/local/bin:/usr/bin',
+require => Package['python3-pip'],
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  require => Package['python3-pip'],
-}
-
-file { '/usr/local/bin/flask':
-  ensure => link,
-  target => '/usr/local/bin/flask',
-  require => Exec['install_flask'],
+exec{ 'install_flask':
+command =>  '/usr/bin/pip3 install flask==2.1.0',
+path    => '/usr/local/bin:/usr/bin',
+require => Exec['install_werkzeug'],
 }
 
