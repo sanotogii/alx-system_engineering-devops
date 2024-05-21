4ca380053f2537328ca1556395f98b36461ab257
sanotogii <nasaereh@gmail.com>
2024-05-21 17:36:54 +0200

    task 0 +4

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,10 +19,10 @@ if __name__ == '__main__':
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(_id)
     user_response = requests.get(user_url)
     user_json = user_response.json()
-    employee = user_json.get('name')
+    employee = user_json.get('name', None)
 
     for item in data:
-        if item.get('userId') == _id:
+        if item.get('userId', None) == _id:
             if item.get('completed') is True:
                 done += 1
                 title.append(item.get("title", None))
