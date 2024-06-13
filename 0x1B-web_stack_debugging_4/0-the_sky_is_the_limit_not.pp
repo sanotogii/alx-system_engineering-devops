ac37d7972459ac34b2c9acb170f947c1c42d9ebb
sanotogii <nasaereh@gmail.com>
2024-06-13 20:01:28 +0200

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,5 @@
 # scaling up the traffic nginx can recieve
 
 exec {'inc':
-	path => '/usr/local/bin/:/bin/',
-	command => 'sed -i "s/15/8000/" /etc/default/nginx && sudo service nginx restart'
-	}
+	path    => '/usr/local/bin/:/bin/',
+	command => 'sed -i "s/15/8000/" /etc/default/nginx && sudo service nginx restart'}
