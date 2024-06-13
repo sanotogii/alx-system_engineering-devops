4e82be251cb84c1ea3f90ad361122b0a86333597
sanotogii <nasaereh@gmail.com>
2024-06-13 20:36:47 +0200

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,6 @@
 # login with the holberton user and open a file without any error message.
 
 exec { 'Change':
-command  => 'sed -i "s/holberton/" /etc/security/limits.conf',
-path     => '/usr/local/bin/:/bin/'
+command => 'sed -i "s/holberton/" /etc/security/limits.conf',
+path    => '/usr/local/bin/:/bin/'
 }
